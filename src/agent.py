74ab0b1d3bc64f6aed54769cece2ac1dd2702a9c
Porter <chien.porter@gmail.com>
2025-12-01 22:26:15 -0700

    added comments to explain each function

--- src/agent.py
+++ src/agent.py
@@ -1,14 +1,21 @@
-import logging
-import json
-import os
+# import os
 import re
 import requests
-from typing import Any,List
-from .tools import search_wiki,search_browser
+from typing import Any
+
+# Custom Module
+from .agent_tools import search_wiki,search_browser
+from .utils import read_file
 
 
 class Agent():
-    def __init__(self, api_key:str, api_base:str, model_name:str,temperature:float):
+    def __init__(
+            self, 
+            api_key: str, 
+            api_base: str, 
+            model_name: str, 
+            temperature: float = 0.0
+    ):
     
         # Model Setting
         self.api_key=api_key
@@ -16,141 +23,195 @@ class Agent():
         self.model_name=model_name
         self.temp=temperature
 
-        # Track Model calls updates everytime call_model is called
+        # Counter updates everytime call_model is called
         self.calls=0
     
-    def self_refine(self, question:str, max_calls:int = 10)->str:
+    def self_refine(self, question: str, max_calls: int = 10) -> str:
+        """
+        Runs the Self-Refine algorithm for a given question.
+
+        The model iteratively generates a response, requests feedback,
+        and refines the response until it achieves a score of 8/10 or if the
+        maximum number of calls is reached.
+
+        Args:
+            question (str): TThe question to ask the model.
+            max_calls (int): Maximum number of refinement calls.
+
+        Returns:
+            str: The refined response that reached 8/10 score,
+                or the last refined response if no acceptable score is met.
+        """
         
-        #Load prompt
-        system_refine_template=self.read_file("./prompts/self_refine/system_refine.txt")
-        system_feedback_template=self.read_file("./prompts/self_refine/system_feedback.txt")
+        #Load system prompt
+        system_refine_template = read_file("./prompts/self_refine/system_refine.txt")
+        system_feedback_template = read_file("./prompts/self_refine/system_feedback.txt")
 
-        score_pattern="([0-9]*/[0-9]*)"
+        score_pattern = "([0-9]*/[0-9]*)"
+        self.calls = 0
 
-        # Creating initial answer
+        # Initial model answer
         user_prompt = f"Question: {question}"
         response = self.call_model(user=user_prompt)
-        self.calls=0
+        
+        for _ in range(max_calls - self.calls):
 
-        for _ in range(max_calls-self.calls):
-
-
-            feedback = self.call_model(user=response["text"],system=system_feedback_template)
+            feedback = self.call_model(
+                user=response["text"],
+                system=system_feedback_template
+            )
 
             if not feedback["text"]:
-                return feedback.get("error")
+                return feedback["error"]
             
+            # Extract score
             match = re.search(score_pattern, feedback["text"])
+            score = match.group(0) if match else None
 
-            # Check the score, return is reach 10 out of 10
-            if match and match.group(0)=="10/10":
+
+            if score == "8/10":
                 answer = self.parse_answer(response["text"])
                 return answer
 
-            # Build the prompt for refining
+            # Build refining prompts
             system_refined = (
                 f"{system_refine_template}"
                 f"Given the feedback {feedback["text"]}\n\n"
                 f"Refine the response below."
             )
-
             user_refined = (
                 f"Orignal question: {question}"
                 f"Your previous response:\n{response["text"]}"
             )
 
-        
-            response_refined = self.call_model(user=user_refined,system=system_refined)
+            response_refined = self.call_model(user=user_refined, system=system_refined)
             
             if not response_refined["text"]:
-                return feedback.get("error")
+                return feedback["error"]
             
-            response=response_refined
+            # Update the refined response to be use for next iteration
+            response = response_refined
 
-        answer = self.parse_answer(response["text"])
-        return answer
+        # If loop completes wihtout achieving 8/10 score
+        return self.parse_answer(response["text"])
         
-    def react(self, question:str, max_calls:int = 20)->str:
-        
-        system_template = self.read_file('./prompts/react/system.txt')
+    def react(self, question: str, max_calls: int = 20) -> str:
+        """ 
+        Runs the ReAct (Reason + Act) algorithm to answer a user question.
 
+        The model iteratively generates reasoning steps and tool calls in the format:
+
+            Thought -> Action -> Input -> Observation
+            (or Previous response if no action is called)
+
+        loops until "Answer:" is produced by the model or max_calls is reached.
+
+        Args:
+            question (str): The question to ask the model.
+            max_calls (int): Maximum number of ReAct calls.
+
+        Returns:
+            str: The model's final answer or "Answer wasn't reach in <20 calls"
+        """   
+
+        system_template = read_file("./prompts/react/system.txt")
+
+        # Map tool (action) name to it's function
         toolbox={
-            'search_wiki':search_wiki,
-            'search_browser': search_browser,
+            "search_wiki": search_wiki,
+            "search_browser": search_browser,
         }
 
-        action_pattern=r"Action: (\w+)"
-        input_pattern=r"Input: (.+)"
+        action_pattern = r"Action: (\w+)"
+        input_pattern = r"Input: (.+)"
         
-        user_prompt=question
-        self.calls=0
+        user_prompt = question
+        self.calls = 0
 
-        for _ in range(max_calls-self.calls):
+        for _ in range(max_calls - self.calls):
 
-            
-            response = self.call_model(user_prompt, system=system_template)
+            response = self.call_model(user=user_prompt, system=system_template)
         
             if not response["text"]:
                 return response.get("error")
   
+            # If an answer has been reach, returns the answer 
             if "Answer:" in response["text"]:
                 answer = self.parse_answer(response["text"])
                 return answer
             
-            action_match = re.search(action_pattern,response["text"])
-            input_match = re.search(input_pattern,response["text"])
-
-            # Determine the action called by the model
+            # Extract model's action and input
+            action_match = re.search(action_pattern, response["text"])
+            input_match = re.search(input_pattern, response["text"])
+  
             action = action_match.group(1) if action_match else None
-            system_input = input_match.group(1) if input_match else None
+            action_input = input_match.group(1) if input_match else None
 
-            # Call tools
+            # Run the action if it is valid
             if action in toolbox:
-                action_response = toolbox[action](system_input)
-                action_response = self.summarize_reasoning(str(action_response))
-
-                user_prompt+=f"\nObservation: {action_response}"
+                action_response = toolbox[action](action_input)
+                action_response = self.summarize_response(str(action_response))
+                user_prompt += f"\nObservation: {action_response}"
                 continue
 
-            # If not action was called,append previous response to continue reasoning
-            previous_response = self.summarize_reasoning(response["text"])
-            user_prompt=f"\nPrevious response: {previous_response}"
+            # no action: continue reasoning using previous response
+            previous_response = self.summarize_response(response["text"])
+            user_prompt = f"\nPrevious response: {previous_response}"
             
         return "Answer wasn't reach in <20 calls"
 
-    def chain_of_thought(self, question:str, max_calls:int = 20)->str:
-        """Utilizes ITerative Chain Reasoning"""
+    def chain_of_thought(self, question: str, max_calls: int = 20) -> str:
+        """
+        Runs iterative chain-of-thought reasoning using repeated model calls.
 
-        #Load prompt
-        system_template=self.read_file('./prompts/chain_of_thought/system.txt')
+        This method iteratively calls the model, each time summarizing the
+        previous reasoning steps and feeding them back as context, until 
+        "Answer:" is produced by the model or max_calls is reached.
+
+        Args:
+            question (str): The question to ask the model.
+            max_calls (int):Maximum number of model calls.
+
+        Returns:
+            str: The model's final answr or "Answer wasn't reach in <20 calls"
+        """
+
+        system_template = read_file("./prompts/chain_of_thought/system.txt")
         
-        # Initialize prompt, chat log, calls
-        user = f"Please solve the following problem step-by-step:\n\nQuestion:{question}"
-        chat_log=[]
-        self.calls=0
+        # Initialize the base user_prompts and empty chat log
+        user_prompt = f"Please solve the following problem step-by-step:\n\nQuestion:{question}"
+        chat_log = []
+        self.calls = 0
 
         for _ in range(max_calls-self.calls):
             
-            response = self.call_model(user,system_template)
+            response = self.call_model(user_prompt,system_template)
 
-            if not response['text']:
-                return response.get("error")
+            if not response["text"]:
+                return response["error"]
             
+            # If an answer has been reach, returns the answer 
             if "Answer" in response["text"]:
-                # Set calls back to zero 
                 answer = self.parse_answer(response["text"])
                 return answer
 
-            chat_log.append(self.summarize_reasoning(response["text"]))
+            # Summarize response to add to chat log
+            chat_log.append(self.summarize_response(response["text"]))
 
-            user = f"Question: {question}\n\nPrevious Reasoning:\n\n{''.join(chat_log)}"
+            # Prepare next user promopt
+            user_prompt = (
+                f"Question: {question}\n\n"
+                f"Previous Reasoning:\n\n{''.join(chat_log)}"
+            )
 
         return "Answer wasn't reach in <20 calls"
 
-    
-    # Shared Components
-
-    def call_model(self, user:str="", system:str="", timeout:int = 30) -> dict[str, Any]:
+    def call_model(
+            self, 
+            user: str = "", 
+            system: str= "", 
+            timeout: int = 30
+    ) -> dict[str, Any]:
         """
         Calls an OpenAI-style /v1/chat/completions endpoint and returns:
         { 'ok': bool, 'text': str or None, 'raw': dict or None, 'status': int, 'error': str or None, 'headers': dict }
@@ -191,11 +252,20 @@ class Agent():
         except requests.RequestException as e:
             return {"ok": False, "text": None, "raw": None, "status": -1, "error": str(e), "headers": {}}
 
-    def summarize_reasoning(self, chat_log:str)->str:
-        """ Summarize the reasoning so far"""
-        summary= self.call_model(
-            chat_log,
-            system="""
+    def summarize_response(self, response: str) -> str:
+        """
+        Summarize the response of the model
+
+        This method calls the model to summarize a text, returning the summarize text
+        and the next step of action.
+
+        Args:
+            response (str): A text that you want to summarize
+        Returns:
+            str: The summarized response
+        """
+
+        system_prompt = """
             You are Qwen, a highly skilled summarization assistant.
 
             Your task:
@@ -207,67 +277,52 @@ class Agent():
             - Do NOT generate a final answer; only produce a summary of the reasoning steps.
             - Output should be in plain text, suitable for feeding as context to the next iteration of problem-solving.
             - Also include the number and task of the next step e.g. Next step: 10 Analyze the function
-            """.strip()
-        )    
+        """.strip()
+
+        summary= self.call_model(user=response, system=system_prompt)    
         
         if not summary["text"]:
-            return summary.get("error")
+            return summary["error"]
 
         return summary["text"]
 
-    def read_file(self, path:str)->str:
+    def parse_answer(self, response: str) -> str:
+        """
+        Parse the answer from the response from a model
 
-        """Reads in txt given a path string"""
-        try:
-            with open(path, 'r') as file:
-                content = file.read()
-            return content
-        
-        except FileNotFoundError:
-            logging.error(f'File not found at {path}')
-            return None
+        This method uses regex to parse out the text after "Answer: ".
 
-    def parse_answer(self,response: str)->str:
-
+        Args:
+            response (str): A text that you want to parse
+        Returns:
+            str: The text after "Answer: "
+        """  
+         
         answer_pattern=r"Answer: (.+)"
 
         match = re.search(answer_pattern,response)
 
-        answer = match.group(1) if match else 'no answer'
+        # Parse the text after "Answer:"
+        answer = match.group(1) if match else "no answer"
 
         return answer
 
-if __name__ == '__main__':
-
-    USER_PATH='../prompts/self_refine/user.txt'
+if __name__ == "__main__":
 
     question=r"""
     In a new school, $40$ percent of the students are freshmen, $30$ percent are sophomores, $20$ percent are juniors, and $10$ percent are seniors. All freshmen are required to take Latin, and $80$ percent of sophomores, $50$ percent of the juniors, and $20$ percent of the seniors elect to take Latin. The probability that a randomly chosen Latin student is a sophomore is $\\frac{m}{n}$ , where $m$ and $n$ are relatively prime positive integers. Find $m+n$ .
     """
-    #869
+    #Answer : 869
     
-    robotucus = Agent(
-        api_key=os.getenv("OPENAI_API_KEY", "cse476"),
-        api_base=os.getenv("API_BASE", "http://10.4.58.53:41701/v1"),
-        model_name=os.getenv("MODEL_NAME", "bens_model"),
-        temperature=0.0
-    )
-
-    print(robotucus.chain_of_thought(question))
-    # print(robotucus.self_refine(question=question))
-    # print(robotucus.read_file(USER_PATH))
-    # for q in robotucus.get_sub_questions(question):
-    #     print(q)
-
-    # for entry in robotucus.action(question):
-    #     print('\n============== Sub Questions ===========\n')
-
-    #     print(entry['subq'],end='\n\n')
-
-    #     print(entry['ans'])
-
-    # print('\n============== Final Answer ===========\n')
+    # robotucus = Agent(
+    #     api_key=os.getenv("OPENAI_API_KEY", "cse476"),
+    #     api_base=os.getenv("API_BASE", "http://10.4.58.53:41701/v1"),
+    #     model_name=os.getenv("MODEL_NAME", "bens_model"),
+    #     temperature=0.0
+    # )
 
     # print(robotucus.chain_of_thought(question))
-    # print(f'# Models Call: {robotucus.calls}')
+    # print(robotucus.self_refine(question=question))
 
+    # system_prompt_path="./prompts/self_refine/system_refine.txt"
+    # print(read_file(system_prompt_path))
